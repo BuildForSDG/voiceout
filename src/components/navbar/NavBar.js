f3c2e4aa381b66fe457667449a080118428a0c40
Toyin Adesina <toyinadesina60@gmail.com>
2020-05-06 01:26:08 +0100

    login layout added together with the hompage and the report components

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -7,47 +7,47 @@ import ReporterSignedInLinks from './ReporterSignedInLinks';
 import VoiceSignedInLinks from './VoiceSignedInLinks';
 
 class NavBar extends Component {
-		constructor(props){
-				super(props);
-		}
+	constructor(props){
+			super(props);
+	}
 		
-		render() {
-				let link = '';
-				let demo = 1;
-				switch (demo) {
-						case 1:
-								link = <SignedOutLinks 
-														handleLoginDisplay={this.props.handleLoginDisplay}
-												/>
-								break;
-						case 2:
-								link = <InstitutionSignedInLinks />
-								break;
-						case 3:
-								link = <ReporterSignedInLinks />
-								break;
-						case 4:
-								link = <VoiceSignedInLinks />
-								break;
-						default:
-								break;
-				}
-				return (
-					<header className='nav-header'>
-						<Container>
-							<Navbar className="color-nav" bg='' expand="lg">
-								<Navbar.Brand href="">
-									<Link className="links brandName" to='/'>VoiceOut</Link>
-								</Navbar.Brand>
-								<Navbar.Toggle aria-controls="basic-navbar-nav" />
-								<Navbar.Collapse id="basic-navbar-nav">
-									{link}
-								</Navbar.Collapse>
-							</Navbar>
-						</Container>
-					</header>
-				)
+	render() {
+		let link = '';
+		let demo = 1;
+		switch (demo) {
+			case 1:
+				link = <SignedOutLinks 
+									handleLoginDisplay={this.props.handleLoginDisplay}
+								/>
+				break;
+			case 2:
+				link = <InstitutionSignedInLinks />
+				break;
+			case 3:
+				link = <ReporterSignedInLinks />
+				break;
+			case 4:
+				link = <VoiceSignedInLinks />
+				break;
+			default:
+				break;
 		}
+		return (
+			<header className='nav-header'>
+				<Container>
+					<Navbar className="color-nav" bg='' expand="lg">
+						<Navbar.Brand href="">
+							<Link className="links brandName" to='/'>VoiceOut</Link>
+						</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						<Navbar.Collapse id="basic-navbar-nav">
+							{link}
+						</Navbar.Collapse>
+					</Navbar>
+				</Container>
+			</header>
+		)
+	}
 }
 
 export default NavBar;
